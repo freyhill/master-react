bd0498f0b772b119fabd3cf1d527268e4f4b2481
leinov <1425795233@qq.com>
2019-01-15 14:57:51 +0800

    react

--- src/key.js
+++ src/key.js
@@ -1,17 +1,13 @@
 /*
- * @Author: leinov 
+ * @author: leinov 
  * @Date: 2019-01-07 15:42:48 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-01-11 10:29:59
+ * @Last Modified time: 2019-01-11 10:31:25
  */
 
 import React, { Component } from 'react';
-
 export default class Key extends Component {
-    
-    
     render() {
-       
         return (
             <div>
             {
@@ -19,7 +15,5 @@ export default class Key extends Component {
             }
             </div>
         )
-
     }
-
 }
